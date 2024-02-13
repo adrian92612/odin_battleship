842a7a58e3a14f95e99051138c9e9f954a7145a4
adrian92612 <adrianvillamin0612@gmail.com>
2024-02-13 17:35:16 +0800

    added gameboard class

--- src/ship.js
+++ src/ship.js
@@ -2,10 +2,11 @@ class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
+    this.sunk = false;
   }
 
   hit = () => this.hits++;
-  isSunked = () => (this.hits < this.length ? false : true);
+  isSunked = () => (this.sunk = this.hits < this.length ? false : true);
 }
 
 export { Ship };
