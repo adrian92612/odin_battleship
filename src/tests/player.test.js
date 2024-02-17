50dbfc1678ea229050165ad26165afed988e8d00
adrian92612 <adrian.villamin0612@gmail.com>
2024-02-17 15:11:21 +0800

    player,comp can attach each other test working

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -5,15 +5,28 @@ import { Ship } from "../ship";
 describe("Player and computer", () => {
   let player;
   let comp;
+  let playerBoard;
+  let compBoard;
   beforeEach(() => {
     player = new Player();
     comp = new Computer();
+    playerBoard = player.gameboard;
+    compBoard = comp.gameboard;
+    playerBoard.placeShip(new Ship(3), 0, 0, true);
+    compBoard.placeShip(new Ship(3), 0, 0, true);
   });
 
   it("be able to place ships", () => {
-    player.gameboard.placeShip(new Ship(3), 0, 0, true);
-    comp.gameboard.placeShip(new Ship(3), 0, 0, true);
-    expect(player.gameboard.grid[0][0] instanceof Ship).toBe(true);
-    expect(comp.gameboard.grid[0][0] instanceof Ship).toBe(true);
+    expect(playerBoard.grid[0][0] instanceof Ship).toBe(true);
+    expect(compBoard.grid[0][0] instanceof Ship).toBe(true);
   });
+
+  it("can attack each other", () => {
+    player.attack(compBoard, 0, 0);
+    comp.attack(playerBoard, 0, 0);
+    expect(playerBoard.grid[0][0] instanceof Ship).toBe(false);
+    expect(compBoard.grid[0][0] instanceof Ship).toBe(false);
+  });
+
+  it("comp can attack entire board w/o repeating", () => {});
 });
