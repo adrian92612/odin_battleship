28ae276a61a2196dd7f452f95e7a57a9d3d66bf2
adrian92612 <adrian.villamin0612@gmail.com>
2024-02-17 17:35:36 +0800

    initial game loop set

--- src/gameloop.js
+++ src/gameloop.js
@@ -1,8 +1,22 @@
 import { Player, Computer } from "./player";
+import { Ship } from "./ship";
 
-function setPlayers() {
-  const human = new Player("Adrian");
-  const comp = new Computer();
+function setPlayers(human, comp) {
+  human = new Player("Adrian");
+  comp = new Computer();
   const humanBoard = human.gameboard;
   const compBoard = comp.gameboard;
+  setShips(humanBoard);
+  setShips(compBoard);
 }
+
+function setShips(board) {
+  board.placeShip(new Ship(4), 0, 0);
+  board.placeShip(new Ship(3), 5, 0, false);
+  board.placeShip(new Ship(3), 1, 5);
+  board.placeShip(new Ship(2), 7, 7, false);
+  board.placeShip(new Ship(1), 6, 5);
+  console.log(board);
+}
+
+export { setPlayers };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import { Ship } from "./ship";
 import { Gameboard } from "./gameboard";
+import { Player, Computer } from "./player";
 
-const ship = new Ship(4);
-const ship2 = new Ship(4);
-const gb = new Gameboard();
-gb.placeShip(ship, 9, 0, false);
-gb.placeShip(ship2, 6, 0, true);
+// const ship = new Ship(4);
+// const ship2 = new Ship(4);
+// const gb = new Gameboard();
+// gb.placeShip(ship, 9, 0, false);
+// gb.placeShip(ship2, 6, 0, true);
 
-gb.receiveAttack(0, 0);
-console.log(gb.grid);
-gb.receiveAttack(1, 0);
-gb.receiveAttack(1, 2);
-gb.receiveAttack(1, 2);
-gb.receiveAttack(1, 2);
+// gb.receiveAttack(0, 0);
+// console.log(gb.grid);
+// gb.receiveAttack(1, 0);
+// gb.receiveAttack(1, 2);
+// gb.receiveAttack(1, 2);
+// gb.receiveAttack(1, 2);
+
+const human = new Player("Human");
+const comp = new Computer();
+const humanBoard = human.gameboard;
+const compBoard = comp.gameboard;
+
+setShips(humanBoard);
+setShips(compBoard);
+
+function setShips(board) {
+  board.placeShip(new Ship(4), 0, 0);
+  board.placeShip(new Ship(3), 5, 0, false);
+  board.placeShip(new Ship(3), 1, 5);
+  board.placeShip(new Ship(2), 7, 7, false);
+  board.placeShip(new Ship(1), 6, 5);
+  console.log(board);
+}
+
+while (humanBoard.isGameOver || compBoard.isGameOver) {}
