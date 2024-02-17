155f47013ba697a0b90338a1fc6aeebce5107e8d
adrian92612 <adrian.villamin0612@gmail.com>
2024-02-17 13:29:19 +0800

    initial player & computer class

---
+++ src/player.js
@@ -0,0 +1,29 @@
+import { Gameboard } from "./gameboard";
+
+class Player {
+  constructor(name) {
+    this.name;
+    this.gameboard = new Gameboard();
+  }
+
+  attack(enemyGameboard, x, y) {
+    enemyGameboard.receiveAttack(x, y);
+  }
+}
+
+class Computer extends Player {
+  constructor() {
+    super("Computer");
+    this.prevAttacks = [];
+  }
+
+  attackPlayer(playerGameboard) {
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * playerGameboard.size);
+      y = Math.floor(Math.random() * playerGameboard.size);
+    } while (this.prevAttacks.includes(`${x},${y}`));
+    this.prevAttacks.push(`${x},${y}`);
+    super.attack(playerGameboard, x, y);
+  }
+}
